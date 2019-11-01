4688e20d94a16c2edaf005da6be43ee7f2b12357
Mark Portnoi <mark.portnoi.12@ucl.ac.uk>
2019-11-01 17:40:38 +0000

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,7 +4,9 @@
 
 int main(int argc,  char * argv[]) {
     loguru::init(argc, argv);
+    loguru::add_file("everything.log", loguru::Append, 2);
     LOG_F(INFO, "Hello from main.cpp!");
+    LOG_F(3,"Wazzup");
     complex_calculation();
     LOG_F(INFO, "main function about to end!");
     return 0;
